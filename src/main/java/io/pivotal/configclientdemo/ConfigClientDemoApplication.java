60361f4b63d8c3fcd21602806f579bd1742ebf41
Simon Rowe <srowe@pivotal.io>
2018-11-28 16:28:55 +0000

    Clean up config client demo.

--- src/main/java/io/pivotal/configclientdemo/ConfigClientDemoApplication.java
+++ src/main/java/io/pivotal/configclientdemo/ConfigClientDemoApplication.java
@@ -1,24 +1,29 @@
 package io.pivotal.configclientdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientDemoApplication.class, args);
 	}
 
-	@Value("${cloudPropertyKey}")
-	private String property;
+	@Autowired
+	private CustomPropertyHolder customPropertyHolder;
 
 	@GetMapping("/")
-	public String cloudProvidedProperty() {
-		return "The property served up by my jdbc backed config server is : " + property;
+	public CustomPropertyHolder properties() {
+		return customPropertyHolder;
 	}
 }
